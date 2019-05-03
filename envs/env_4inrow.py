2de46908939980216ac8ba341424b5734bbabad4
David6114 <49832787+David6114@users.noreply.github.com>
2019-05-03 13:12:47 +0300

    Update env_4inrow.py

--- envs/env_4inrow.py
+++ envs/env_4inrow.py
@@ -7,31 +7,40 @@ class Env4InRow(Environment):
     Args:
         player1, player2: the players
         board_size (tuple): the size of the board.
-            board_size[0]: height
-            board_size[1]: width
+            boardH: height
+            boardW: width
     """
 
     def __init__(self, player1, player2, board_size):
         super(Env4InRow, self).__init__(player1, player2)
-        self.board_size = board_size
-        self.board = [[0] * board_size[0]] * board_size[1]
+        self.player1 = player1
+        self.player2 = player2
+        self.boardH = board_size[0]
+        self.boardW = board_size[1]
+        self.board = [[0] * self.boardW] * self.boardH
 
     def reset(self):
-        self.board = [[0] * self.board_size[0]] * self.board_size[1]
+        self.board = [[0] * self.boardW] * self.boardH
+        return self.board
 
     def apply_action(self, player, action):
         if action in self.available_moves(player):
-            self.board[action[0]][action[1]] = player["symbol"]
+            self.board[action[0]][action[1]] = player
             # next state, reward
             return self.board, 0
-        print("Wrong action")
+        return None
 
     def render(self):
         for i in self.board:
             print(i)
 
     def available_moves(self, player):
-        pass
+        moves = []
+        for i in range(self.boardW):
+            for j in reversed(range(self.boardH)):
+                if self.board[j][i] == 0:
+                    moves.append((j, i))
+        return moves
 
     def is_terminal_state(self):
         for player in [self.player1, self.player2]:
