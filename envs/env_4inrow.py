e0e2505359f73735b770c2ff7c10e10a2618e95d
David6114 <49832787+David6114@users.noreply.github.com>
2019-05-04 21:21:54 +0300

    Adding the "player_status()" func

--- envs/env_4inrow.py
+++ envs/env_4inrow.py
@@ -37,17 +37,59 @@ class Env4InRow(EnvironmentBase):
 
     def available_moves(self, player):
         moves = []
-        for i in range(self.boardW):
-            for j in reversed(range(self.boardH)):
-                if self.board[j][i] == 0:
-                    moves.append((j, i))
+        for j in range(self.boardW):
+            for i in reversed(range(self.boardH)):
+                if not self.board[i][j]:
+                    moves.append((i, j))
         return moves
 
     def is_terminal_state(self):
         for player in [self.player1, self.player2]:
             if self.available_moves(player) == [] \
-                    or self.player_status(player) != 0:
+                    or self.player_status(player):
                 return True
 
     def player_status(self, player):
-        pass
+
+        # Iteration on each cell
+        for i in reversed(range(self.boardH)):
+            for j in range(self.boardW):
+
+                # check if not zero and remember the cell symbol
+                if self.board[i][j]:
+                    symbol = self.board[i][j]
+
+                    # check four in row: up
+                    if (i-IN_ROW+1) >= 0 \
+                            and all(self.board[i-k][j] == symbol for k in range(1, IN_ROW)):
+                        if symbol == player:
+                            return 1
+                        else:
+                            return -1
+
+                    # check four in row: up-right
+                    if (i-IN_ROW+1) >= 0 \
+                            and (j+IN_ROW-1) < self.boardW \
+                            and all(self.board[i-k][j+k] == symbol for k in range(1, IN_ROW)):
+                        if symbol == player:
+                            return 1
+                        else:
+                            return -1
+
+                    # check four in row: right
+                    if (j+IN_ROW-1) < self.boardW \
+                            and all(self.board[i][j+k] == symbol for k in range(1, IN_ROW)):
+                        if symbol == player:
+                            return 1
+                        else:
+                            return -1
+
+                    # check four in row: down-right
+                    if (i+IN_ROW-1) < self.boardH \
+                            and (j+IN_ROW-1) < self.boardW \
+                            and all(self.board[i+k][j+k] == symbol for k in range(1, IN_ROW)):
+                        if symbol == player:
+                            return 1
+                        else:
+                            return -1
+        return 0
