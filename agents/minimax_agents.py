a057dcadb9f606043db00033856d84b71884df75
David6114 <dglukhov99@gmail.com>
2019-08-06 18:40:23 +0300

    pix some bags in minimax algorithm

--- agents/minimax_agents.py
+++ agents/minimax_agents.py
@@ -1,6 +1,7 @@
 from .agent_base import Agent
 # Note - from this point on, we refer to the book 'Artificial Intelligence: a Modern Approach' as AIMA.
 
+from threading import Timer
 DEFAULT_SEARCH_DEPTH = 3  # Maximum depth of the search
 TIMEOUT = 10.  # Maximum time for the agent to search through the state space
 
@@ -26,6 +27,7 @@ class SearchAgentBase(Agent):
     """
     Base class for searching agents, like minimax and alpha-beta pruning agents.
     """
+
     def __init__(self, name, search_depth=DEFAULT_SEARCH_DEPTH, score_fn=default_score_fn, timeout=TIMEOUT):
         super(SearchAgentBase, self).__init__(name)
         self.search_depth = search_depth
@@ -40,32 +42,35 @@ class MinimaxAgent(SearchAgentBase):
     """
     This agent implements the minimax algorithm, described in the book AIMA (3rd edition): Chapter 5.2.
     """
+    @staticmethod
+    def timeout_error():
+        """function to arise TimeError"""
+        raise TimeoutError
 
-    def minimax(self, node, search_depth, max_agent = False):
-        
+    def minimax(self, node, depth, max_turn=False):
+
         # check terminal state or max search depth
-        if node.is_terminal_state or search_depth == 0:
+        if node.is_terminal_state() or depth == 0:
             return self.score_fn(node, self)
 
         # return MAX value of children nodes
-        if max_agent:
+        if max_turn:
             val = -float("inf")
-            for move in node.available_moves:
-                new_node = node.copy()
-                new_node.applay_action(self, move)
-                val = max(val, self.minimax(new_node, search_depth - 1, max_agent = False))
+            for move in node.available_moves(self):
+                child = node.copy()
+                child.applay_action(self, move)
+                val = max(val, self.minimax(child, depth - 1, max_turn=False))
 
         # return MIN value of children nodes
         else:
             val = float("inf")
             node.switch_player()
-            for move in node.available_moves:
-                new_node = node.copy()
-                new_node.applay_action(self, move)
-                val = min(val, self.minimax(new_node, search_depth - 1, max_agent = True))
+            for move in node.available_moves(self):
+                child = node.copy()
+                child.applay_action(self, move)
+                val = min(val, self.minimax(child, depth - 1, max_turn=True))
 
         return val
-
 
     def choose_action(self, env):
 
@@ -75,22 +80,27 @@ class MinimaxAgent(SearchAgentBase):
         # check if we have available moves at all
         if available_moves:
 
+            # start timeout timer
+            timer = Timer(TIMEOUT, self.timeout_error())
+            timer.start()
+
             # search for a best move with the best value in the next node
-            best_move = {"move":None, "val":-float("inf")}
+            best_move = {"move": None, "val": -float("inf")}
             for move in available_moves:
-                new_node = env.copy()
-                new_node.applay_action(move)
-                val = self.minimax(new_node, self.search_depth - 1, max_agent=False)
+                child = env.copy()
+                child.applay_action(self, move)
+                val = self.minimax(
+                    child, self.search_depth - 1, max_turn=False)
                 if val > best_move["val"]:
                     best_move["move"] = move
                     best_move["val"] = val
+            timer.cancel()
 
             # return the best move
             return best_move["move"]
 
         # if we don't have available moves return None
         return None
-        
 
 
 class AlphaBetaPruningAgent(SearchAgentBase):
@@ -98,5 +108,6 @@ class AlphaBetaPruningAgent(SearchAgentBase):
     This agent implements the alpha-beta pruning algorithm, described in the book AIMA (3rd edition): Chapter 5.3.
     TODO - Implement!
     """
+
     def choose_action(self, env):
         pass
